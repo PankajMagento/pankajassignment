ded514065bd6fc74c7bc43c359ae3d0d61ff5173
PankajMagento <167745466+PankajMagento@users.noreply.github.com>
2024-04-26 13:03:58 +0530

    Create a custom drop down attribute

---
+++ ProductType/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @category   Technical Assessment
+ * @package    PankajAssignment_ProductType
+ * @author     pankaj371997@gmail.com 
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PankajAssignment_ProductType',
+    __DIR__
+);
