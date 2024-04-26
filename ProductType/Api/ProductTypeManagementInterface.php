e1abd957c7b3257c3e783893bef940cab50547f4
PankajMagento <167745466+PankajMagento@users.noreply.github.com>
2024-04-26 18:38:52 +0530

    Update ProductTypeManagementInterface.php

--- ProductType/Api/ProductTypeManagementInterface.php
+++ ProductType/Api/ProductTypeManagementInterface.php
@@ -8,5 +8,13 @@ namespace PankajAssignment\ProductType\Api;
 
 interface ProductTypeManagementInterface
 {
+    /**
+     * Set custom attribute dropdown value for a product based on its SKU
+     *
+     * @param string $sku
+     * @param string $attributeCode
+     * @param string $value
+     * @return array
+     */
     public function setProductType(string $sku, string $attributeCode, string $value): array;
 }
